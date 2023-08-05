7fcb6cc73851e15753ce0149d4d7ed28d10c7ed2
alerodriguezn <navarrocmo@gmail.com>
2023-08-05 10:14:16 -0600

    Fix exersice 3, includes pointers

--- Imperative/Solution-3/main.go
+++ Imperative/Solution-3/main.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 )
 
-func rotateArray(list []string, movements int, direction bool) []string {
-	length := len(list)
+func rotateArray(list *[]string, movements int, direction bool) []string {
+	length := len(*list)
 	var aux = make([]string, length)
 	for i := 0; i < length; i++ {
 		if direction {
 			if (i + movements) >= length {
-				aux[(i+movements)-length] = list[i]
+				aux[(i+movements)-length] = (*list)[i]
 			} else {
-				aux[i+movements] = list[i]
+				aux[i+movements] = (*list)[i]
 			}
 		} else {
 			if (i - movements) < 0 {
-				aux[(i-movements)+length] = list[i]
+				aux[(i-movements)+length] = (*list)[i]
 			} else {
-				aux[i-movements] = list[i]
+				aux[i-movements] = (*list)[i]
 			}
 		}
 
@@ -29,8 +29,9 @@ func rotateArray(list []string, movements int, direction bool) []string {
 func main() {
 
 	elements := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+
 	// True = Right | False = Left
-	arrayRotated := rotateArray(elements, 3, false)
+	arrayRotated := rotateArray(&elements, 3, false)
 	fmt.Print(arrayRotated)
 
 }
